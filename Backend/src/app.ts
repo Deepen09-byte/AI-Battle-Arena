f176b2cb637a6ac21d0d384e863318f6c37eccb1
Deepen09-byte <deepen2918@gmail.com>
2026-09-14 16:59:46 +0530

    Restarted

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,15 +1,9 @@
-import express from "express"
-import useGraph from "./services/graph.ai.js"
+import express from "express";
 
 const app = express()
 
-app.get('/health', (req, res) => {
-    res.status(200).json({ status:'ok'})
+app.get('/', (req, res) => {
+    res.send('Hello Mf')
 })
-
-app.post('/use-graph',async (req, res) => {
-    await useGraph("what is the fuckin capital of fuckin antartica?")
-})
-
 
 export default app
